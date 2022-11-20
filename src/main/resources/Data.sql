db6cd5398db3cbbb64d0bffe645e7eef14588fc2
gwynethvondee <gwynethvondee@gmail.com>
2022-11-20 22:47:37 +0000

    Added imports

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO RECIPE VALUES (1,'Special fried rice','Rice, Egg, Soy sauce, ')
+INSERT INTO RECIPE VALUES (2,'Spaghetti Bolognese','Beef,Tomato,Onion & Garlic,Bay leaves,Pasta')
+INSERT INTO RECIPE VALUES (3,'Chicken Katsu','Chicken, Salt, Egg, Oil')
+INSERT INTO RECIPE VALUES (4,'Pasta with salmon & peas','Salmon, Fusilli, Peas, Low fat cream, Butter')
+INSERT INTO RECIPE VALUES (4,'Thai red curry','Coconut milk, Jasmine rice, Coconut milk, Red curry paste')
