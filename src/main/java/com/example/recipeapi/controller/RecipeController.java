6af5094ee45818c5803d800a840e70590b53b274
gwynethvondee <gwynethvondee@gmail.com>
2022-11-24 15:20:44 +0000

    Final changes

--- src/main/java/com/example/recipeapi/controller/RecipeController.java
+++ src/main/java/com/example/recipeapi/controller/RecipeController.java
@@ -1,13 +1,14 @@
 package com.example.recipeapi.controller;
 
 
-
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 import com.example.recipeapi.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.recipeapi.repository.RecipeRepository;
 import com.example.recipeapi.entity.Recipe;
 
 
--- src/main/java/com/example/recipeapi/entity/Recipe.java
+++ src/main/java/com/example/recipeapi/entity/Recipe.java
@@ -1,27 +1,60 @@
 package com.example.recipeapi.entity;
 
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 
 @Entity
-@Table(name = "Recipe_Table")
 public class Recipe {
 
-@Id
-@GeneratedValue
+    @Id
+    @GeneratedValue()
     private int Id;
+
+
     private String name;
+
+
     private String ingredients;
-    private String instructions; }
+
+    private String instructions;
+public Recipe (){
+
+}
+
+    public Recipe(Long Id,String name, String ingredients, String instructions) {
+        super ();
+        this.name = name;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+}
+
+    public int getId() {
+        return Id;
+    }
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+    public String getName() {
+      return name;
+    }
+    public void setName(String Name) {
+        this.name = name;
+    }
+    public String getIngredients() {
+        return ingredients;
+    }
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+    public String getInstructions(){
+        return instructions;
+    }
+    public void setInstructions(String instructions) {
+        this.ingredients = instructions;
+    }
+
+}
